8770b398e1e70a0a8a1ecaef9bcccbb9adef51ef
dustxyz <dustxyz@criptext.com>
2021-04-18 13:10:37 -0400

    update

--- setup/android_build_env.sh
+++ setup/android_build_env.sh
@@ -35,24 +35,19 @@ sudo DEBIAN_FRONTEND=noninteractive \
     apt install \
     adb autoconf automake axel bc bison build-essential \
     ccache clang cmake expat fastboot flex g++ \
-    g++-multilib gawk gcc gcc-multilib git gnupg gperf \
+    g++-multilib gawk gcc gcc-multilib make gnupg gperf \
     htop imagemagick lib32ncurses5-dev lib32z1-dev libtinfo5 libc6-dev libcap-dev \
     libexpat1-dev libgmp-dev '^liblz4-.*' '^liblzma.*' libmpc-dev libmpfr-dev libncurses5-dev \
     libsdl1.2-dev libssl-dev libtool libxml2 libxml2-utils '^lzma.*' lzop \
     maven ncftp ncurses-dev patch patchelf pkg-config pngcrush \
     pngquant python2.7 python-all-dev re2c schedtool squashfs-tools subversion \
     texinfo unzip w3m xsltproc zip zlib1g-dev lzip \
-    libxml-simple-perl apt-utils gh \
+    libxml-simple-perl apt-utils \
     ${PACKAGES} -y
 
 
-if [[ "$(command -v make)" ]]; then
-    makeversion="$(make -v | head -1 | awk '{print $3}')"
-    if [[ ${makeversion} != "${LATEST_MAKE_VERSION}" ]]; then
-        echo "Installing make ${LATEST_MAKE_VERSION} instead of ${makeversion}"
-        bash "$(dirname "$0")"/make.sh "${LATEST_MAKE_VERSION}"
-    fi
-fi
 
-echo "Installing repo"
-apt-get install repo
+
+
+
+
